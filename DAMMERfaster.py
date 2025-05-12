b8011960be5a02e735e057860126b25a229bc86d
JaMerckx <jannes.merckx@uantwerpen.be>
2025-05-12 19:29:55 +0200

    Update DAMMERfaster.py

--- DAMMERfaster.py
+++ DAMMERfaster.py
@@ -1987,7 +1987,99 @@ def Startingclusterfunction(attenuation, vertices, triangles, sinogram, systemma
 
     startvalue = 1
 
-    scalerseg = startvalue*projdif0/(2*len(np.where((neighbors).ravel() > -0.5)[0]))
+    scalerseg = 0.5*startvalue*projdif0/(2*len(np.where((neighbors).ravel() > -0.5)[0]))
+    simplificdegree = 10
+    nummax = 100
+    attenuationor = attenuation.copy()
+    connectionarray = np.arange(len(triangles))
+    while len(np.unique(connectionarray)) > np.max(np.array([len(triangles)/simplificdegree, nummax])):
+     scalerseg = scalerseg*2
+
+
+     connectionarray = np.arange(len(triangles))
+     projdif0 = np.sum((sinogram - systemmat@attenuation)**2)
+     numangles = int(len(sinogram)/numpix)
+     startvalue = 1
+     con_dict = {}
+     for c in range(len(triangles)):
+         con_dict[c] = [c]
+
+     areas = [Polygon(vertices[t]).area for t in triangles]
+     areas = np.array(areas)
+     trianglist = np.zeros((len(np.where((neighbors) > -0.5)[0]), 2))
+     trianglist[:, 0] = np.where((neighbors) > -0.5)[0]
+     trianglist[:, 1] = neighbors[np.where((neighbors) > -0.5)]
+     trianglist = trianglist.astype(int)
+     attenuation = attenuationor.copy()
+     gradoveredges = np.abs(attenuation[trianglist[:,0]] - \
+                    attenuation[trianglist[:, 1]])
+     projdif0ns = systemmat@attenuation - sinogram  
+
+     lengrad = len(gradoveredges)/2
+     funpos0 = projdif0 + scalerseg*lengrad
+     simplificdegree = 10
+     nummax = 100
+     maxgrad = np.max(gradoveredges) 
+     gradorder = np.argsort(gradoveredges)
+     compared = np.zeros((2, 2))
+     for gind in range(len(gradoveredges)):
+        mingrad = gradorder[gind]#np.argmin(gradoveredges)
+        triangle1 = trianglist[mingrad][0]
+        triangle2 = trianglist[mingrad][1]
+        if connectionarray[triangle1] == connectionarray[triangle2]:
+           gradoveredges[mingrad] = maxgrad
+           continue
+        tmin = np.min(connectionarray[np.array([triangle1, triangle2])])
+        tmax = np.max(connectionarray[np.array([triangle1, triangle2])])
+        con1 = con_dict[connectionarray[triangle1]]
+        con2 = con_dict[connectionarray[triangle2]]
+        if len(con1) > 10 and len(con2) > 10:
+           if np.abs(attenuation[triangle1] - attenuation[triangle2]) > 0.1:
+               continue
+       
+        wheremin = np.where(compared[:, 0] == tmin)[0]
+        if len(wheremin) > 0:
+           wheremax = np.where(compared[wheremin, 1] == tmax)[0]
+           if len(wheremax) > 0:
+             continue
+        atprev = attenuation[np.hstack((con1, con2))].copy()
+        attenuation[np.hstack((con1, con2))] = np.sum(attenuation[np.hstack((con1, con2))]* \
+                                   areas[np.hstack((con1, con2))])/np.sum(areas[np.hstack((con1, con2))])
+
+        lennext = 0
+        for con in neighbors[con1].ravel():
+          if con in con2:
+              lennext += 1
+            
+
+        projdif0ns = projdif0ns + systemmat[:, np.hstack((con1, con2))]@(attenuation[np.hstack((con1, con2))]- atprev)
+        funpos = np.sum(projdif0ns**2) + scalerseg*(lengrad - lennext)
+        if funpos < funpos0:
+           funpos0 = funpos
+           lengrad -= lennext
+           minval = np.min(connectionarray[np.array([triangle1, triangle2])])
+           otherval = np.max(connectionarray[np.array([triangle1, triangle2])])
+           connectionarray[np.hstack((con1, con2))] = minval
+           con_dict[minval].extend(con_dict[otherval]) 
+           del con_dict[otherval]
+           compared = np.zeros((2, 2))
+        else:   
+            projdif0ns = projdif0ns - systemmat[:, np.hstack((con1, con2))]\
+            @(attenuation[np.hstack((con1, con2))]- atprev)
+            attenuation[np.hstack((con1, con2))] = atprev
+            compared = np.vstack((compared, np.array([tmin, tmax])))
+       #gradoveredges[mingrad] = maxgrad
+
+    
+    return attenuation, connectionarray, scalerseg 
+
+
+def Startingclusterfunctionf(attenuation, vertices, triangles, sinogram, systemmat, neighbors):
+    projdif0 = np.sum((sinogram - systemmat@attenuation)**2)
+
+    startvalue = 1
+
+    scalerseg = 0.5*startvalue*projdif0/(2*len(np.where((neighbors).ravel() > -0.5)[0]))
     simplificdegree = 10
     nummax = 100
     attenuationor = attenuation.copy()
@@ -2899,11 +2991,10 @@ for optsteps in range(10):
              verticesopt = vertices.copy()
              trianglesopt = triangles.copy()
              attenuationopt = attenuation.copy()
-
-
+     
      if optsteps == 9:
-         break    
-    
+         break        
+     
      vertices, triangles, attenuation = flipandcolclose(vertices, triangles, res, trianglesint, edges, attenuation)
 
      vertices, triangles, attenuation = edgecollapse(vertices, triangles, attenuation, minvar, qmax, 50000, 0, qmax)
@@ -2928,6 +3019,11 @@ ax.tripcolor(triang, attenuationopt, cmap='grey')
 plt.axis("off")
 plt.show()
 
+fig, ax = plt.subplots(figsize=(9,9))
+triang = tri.Triangulation(vertices[:, 0], vertices[:, 1], triangles)
+ax.tripcolor(triang, attenuation, cmap='grey')
+plt.axis("off")
+plt.show()
 
 # %%
 #With noise
@@ -3039,10 +3135,10 @@ for optsteps in range(10):
              verticesopt = vertices.copy()
              trianglesopt = triangles.copy()
              attenuationopt = attenuation.copy()
-
+     
      if optsteps == 9:
-         break
-    
+         break     
+
      vertices, triangles, attenuation = flipandcolclose(vertices, triangles, res, trianglesint, edges, attenuation)
 
      vertices, triangles, attenuation = edgecollapse(vertices, triangles, attenuation, minvar, qmax, 50000, 0, qmax)
@@ -3066,3 +3162,12 @@ triang = tri.Triangulation(verticesopt[:, 0], verticesopt[:, 1], trianglesopt)
 ax.tripcolor(triang, attenuationopt, cmap='grey')
 plt.axis("off")
 plt.show()
+
+
+fig, ax = plt.subplots(figsize=(9,9))
+triang = tri.Triangulation(vertices[:, 0], vertices[:, 1], triangles)
+ax.tripcolor(triang, attenuation, cmap='grey')
+plt.axis("off")
+plt.show()
+
+# %%
